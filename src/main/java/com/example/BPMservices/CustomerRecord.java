ee4aaf63c578b1cc1650eae36b2ed9037c7db1e0
Ibrahim <rmibrahim00@gmail.com>
2022-11-28 12:18:49 +0500

    init.

---
+++ src/main/java/com/example/BPMservices/CustomerRecord.java
@@ -0,0 +1,74 @@
+package com.example.BPMservices;
+
+import java.sql.Array;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRecord {
+    
+    private int Id;
+    
+    private int ExcelID;
+    private Timestamp CreatedAt;
+    private String CreatedBy;
+    private String CustomerData;
+    private String Status;
+    public CustomerRecord(int id, int excelID, Timestamp createdAt, String createdBy, String customerData, String status) {
+        Id = id;
+        ExcelID = excelID;
+        CreatedAt = createdAt;
+        CreatedBy = createdBy;
+        CustomerData = customerData;
+        Status = status;
+    }
+
+    public CustomerRecord(int excelID) {
+        ExcelID = excelID;
+    }
+    
+
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public int getExcelID() {
+        return ExcelID;
+    }
+    public void setExcelID(int excelID) {
+        ExcelID = excelID;
+    }
+    public Timestamp getCreatedAt() {
+        return CreatedAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        CreatedAt = createdAt;
+    }
+    public String getCreatedBy() {
+        return CreatedBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        CreatedBy = createdBy;
+    }
+    public String getCustomerData() {
+        return CustomerData;
+    }
+    public String[] getCustomerDetails() {
+        return CustomerData.split("\\^");
+    }    
+    public void setCustomerData(String customerData) {
+        CustomerData = customerData;
+    }
+    public String getStatus() {
+        return Status;
+    }
+    public void setStatus(String status) {
+        Status = status;
+    }
+    
+
+}
---
+++ src/test/java/com/example/BPMservices/BpmServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BPMservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
